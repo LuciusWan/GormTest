f035edc1562761a204c2e571b8b0a1897500bbfc
LuciusWan <18099488938@163.com>
2025-01-18 00:14:27 +0800

    Gorm的基础crud

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
-func main() {
-
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func main() {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/test11?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("连接数据库失败：" + err.Error())
+	}
+
+	// 迁移 schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// Select
+	var product Product
+	db.First(&product, 1)                 // 根据整型主键查找
+	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+
+	// Update - 将 product 的 price 更新为 200
+	db.Model(&product).Update("Price", 200)
+	// Update - 更新多个字段
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - 删除 product
+	db.Delete(&product, 1)
+}
